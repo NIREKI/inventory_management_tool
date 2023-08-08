6d93364f38f2e5c11ee3eb05db0dce640f828c08
Niklas <niklas.kinze@gmail.com>
2023-08-08 22:03:25 +0200

    added AppBar to new screens

--- lib/createContract.dart
+++ lib/createContract.dart
@@ -5,14 +5,11 @@ class createContract extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      body: Row(children: [
-        BackButton(onPressed: () => Navigator.pop(context)),
-        Text(
-          "Einen neuen Vertrag anlegen",
-          style: theme.textTheme.displaySmall,
-          textAlign: TextAlign.center,
-        )
-      ]),
+      appBar: AppBar(
+          backgroundColor: theme.colorScheme.primary,
+          title: Text("Einen neuen Vertrag anlegen",
+              style: TextStyle(color: Colors.white))),
+      body: Column(children: [Row()]),
     );
   }
 }
